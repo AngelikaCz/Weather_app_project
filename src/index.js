6b91a550f06f67d7622acd89d3dc8bfafb8dbe61
AngelikaCz <angelikaczerwinska9@gmail.com>
2022-08-07 12:44:16 +0200

    Added cuurent time and day

--- src/index.js
+++ src/index.js
@@ -13,4 +13,23 @@ function displayWeather(data) {
   displayedWind.innerHTML = Math.round(data.data.wind.speed);
   let displayedDescription = document.querySelector("#description");
   displayedDescription.innerHTML = data.data.weather[0].description;
+  let displayedDate = document.querySelector("#date");
+  displayedDate.innerHTML = getDate(data.data.dt * 1000);
+}
+
+function getDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
 }
